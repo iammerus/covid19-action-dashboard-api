e7b8a5951f25675fe2206241333e2e1ed2a068ad
Melvin Mupondori <melmups@outlook.com>
2020-04-09 18:27:06 +0200

    Added routes for getting statistics and statistics history

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ Route::group(['prefix' => 'v1'], function() {
     // Country
     Route::get('/country', "CountryController@index");
 
-
-
+    Route::get('/statistics', "StatisticsController@index");
+    Route::get('/statistics/history', "StatisticsController@history");
 });
