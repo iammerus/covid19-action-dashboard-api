b4053afd4662f38644a5ce8941c50168cedd3f7d
Melvin Mupondori <melmups@outlook.com>
2020-04-09 15:42:19 +0200

    Added relationship to country in DailyStatistic model

--- app/DailyStatistic.php
+++ app/DailyStatistic.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class DailyStatistic extends Model
 {
-    //
+    /**
+     * Gets the country which owns the daily statistic
+     */
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
