38b9c618d9c19c060599434ed81651f024fbad2c
Melvin Mupondori <melmups@outlook.com>
2020-04-09 18:26:00 +0200

    Hid colummns from DailyStatistic serialization

--- app/DailyStatistic.php
+++ app/DailyStatistic.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class DailyStatistic extends Model
 {
+    protected $hidden = ['created_at', 'updated_at', 'country_id'];
     /**
      * Gets the country which owns the daily statistic
      */
