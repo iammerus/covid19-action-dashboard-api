402807ee2130a95cf5e2c9bb02831e370d30c0b5
Melvin Mupondori <melmups@outlook.com>
2020-04-09 17:46:42 +0200

    Cleaned up the fetch data command

--- app/Console/Commands/FetchDataCommand.php
+++ app/Console/Commands/FetchDataCommand.php
@@ -43,7 +43,7 @@ class FetchDataCommand extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      * @throws \Exception
      */
     public function handle()
@@ -51,32 +51,40 @@ class FetchDataCommand extends Command
         $this->alert("Fetching latest statistics data");
         $this->line('');
 
-
+        // Fetch the latest JSON file with the data
         $latest = $this->fetchLatestFile();
-        $this->line('');
 
+        // Get the date of the last entry
         $lastDate = DailyStatistic::query()->orderByDesc('date')->first();
 
-        if($lastDate) $lastDate = $lastDate->date;
-        else $lastDate = date('Y-m-d');
+        // If we got a record, then set last date to that record's date, else set thee last date to a date in the past
+       $lastDate = $lastDate ? new DateTime($lastDate->date) : (new DateTime('2000-01-01'));
 
-        $this->info("Inserting records after {$lastDate}");
+       // Print some information out to the screen
+        $this->info("Inserting records after {$lastDate->format('Y-m-d')}");
+        $this->line('');
 
-        $countries = Country::all();
-
+        // Unguard the daily statistic model
         DailyStatistic::unguard();
-
-        $lastDate = new DateTime($lastDate);
-
-        foreach ($countries as $country) {
-
+        foreach (Country::all() as $country) {
+            // If there's no entries for the country, ignore it
             if(!array_key_exists($country->name, $latest)) continue;
 
-            $collection = collect($latest[$country->name])->reverse();
+            // Create a new laravel collection from the data array, then reverse it
+            // Since the entries are in ascending order, well reverse the array, so the latest dates come early
+            // That way, we get to the last entry we inserted quicker
+            $countryData = collect($latest[$country->name])->reverse();
 
-            foreach ($collection as $data) {
+            // Counter variable.
+            $total = 0;
+
+            // Loop over the country's records
+            foreach ($countryData as $data) {
+                // If the date we are on is the last date, that means we've inserted all the records we needed to
+                // insert for this country, break out of the loop
                 if(!(new DateTime($data['date']))->diff($lastDate)->d) break;
 
+                // Insert the statistic record
                 DailyStatistic::create([
                     'country_id' => $country->id,
                     'date' => $data['date'],
@@ -84,10 +92,19 @@ class FetchDataCommand extends Command
                     'deaths' => $data['deaths'],
                     'recovered' => $data['recovered']
                 ]);
+
+                $total++;
             }
+
+            $this->comment("Finished inserting records for {$country->name}. Total records: {$total}");
         }
     }
 
+    /**
+     * Fetches the latest records
+     *
+     * @return array
+     */
     private function fetchLatestFile()
     {
         $this->comment("Downloading file from '{$this->url}'...");
@@ -100,7 +117,9 @@ class FetchDataCommand extends Command
         // Store the file, use a little trick to minify the output
         file_put_contents($this->path, json_encode(json_decode($json)));
 
+        // Print out a pretty message
         $this->comment("Download complete.");
+        $this->line('');
 
         return json_decode($json, true);
     }
