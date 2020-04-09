40ab4cadaca963e220c5c4ea27c3fe6ab6e2510e
Melvin Mupondori <melmups@outlook.com>
2020-04-09 04:19:35 +0200

    Fleshed out DailyStatisticTableSeeder

--- database/seeds/DailyStatisticsTableSeeder.php
+++ database/seeds/DailyStatisticsTableSeeder.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Country;
+use App\DailyStatistic;
 use Illuminate\Database\Seeder;
 
 class DailyStatisticsTableSeeder extends Seeder
 {
+    public function __construct() {
+        $json = file_get_contents(__DIR__ . '/data/timeseries.json');
+
+        $this->data = json_decode($json);
+    }
     /**
      * Run the database seeds.
      *
@@ -11,6 +18,22 @@ class DailyStatisticsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $countries = Country::all();
+
+        DailyStatistic::unguard();
+
+        foreach ($countries as $country) {
+            foreach ($this->data[$country->name] as $data) {
+                DailyStatistic::create([
+                    'country_id' => $country->id,
+                    'date' => $data->date,
+                    'confirmed' => $data->confirmed,
+                    'deaths' => $data->deaths,
+                    'recovered' => $data->recovered
+                ]);
+            }
+        }
     }
+
+    private $data = [];
 }
