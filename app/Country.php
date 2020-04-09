9b65146e07b8d87824a605a92d51c04f42dc2328
Melvin Mupondori <melmups@outlook.com>
2020-04-09 18:25:23 +0200

    Hid some columns from serialization :

--- app/Country.php
+++ app/Country.php
@@ -8,6 +8,8 @@ class Country extends Model
 {
     protected $fillable = ['region', 'name', 'code'];
 
+    protected $hidden = ['id', 'created_at', 'updated_at'];
+
     public function statistics()
     {
         return $this->hasMany(DailyStatistic::class);
