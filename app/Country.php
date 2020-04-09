636c2b04e0f84cf2f65024d3061bf28617c070c3
Melvin Mupondori <melmups@outlook.com>
2020-04-09 15:46:43 +0200

    Removed all references to total. As it is unnecessary

--- app/Country.php
+++ app/Country.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     protected $fillable = ['region', 'name', 'code'];
+
+    public function statistics()
+    {
+        return $this->hasMany(DailyStatistic::class);
+    }
 }
