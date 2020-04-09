a8736d5774d6e35d559bf8b4f40cc3250abe6161
Melvin Mupondori <melmups@outlook.com>
2020-04-09 18:27:45 +0200

    Added methods for getting single country statistics, all country statistics

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Country;
 use App\DailyStatistic;
-use Illuminate\Http\Request;
 
-class DailyStatisticController extends Controller
+class StatisticsController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,72 +14,82 @@ class DailyStatisticController extends Controller
      */
     public function index()
     {
-        //
+        $results = [];
+        $countries = Country::all();
+
+        foreach ($countries as $country) {
+            // Get the latest data for the country
+            $latest = DailyStatistic::whereCountryId($country->id)->orderByDesc('date')->first();
+
+            if (!$latest) continue;
+
+            $results[] = [
+                'country' => $country->name,
+                'code' => $country->code,
+                'region' => $country->region,
+                'data' => [
+                    'recovered' => $latest->recovered,
+                    'deaths' => $latest->deaths,
+                    'confirmed' => $latest->confirmed
+                ]
+            ];
+        }
+
+        return response($results);
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function history()
     {
-        //
+        $results = [];
+        $countries = Country::all();
+
+        foreach ($countries as $country) {
+            // Get the latest data for the country
+            $latest = DailyStatistic::whereCountryId($country->id)->orderByDesc('date')->limit(20)->get();
+
+            if (!$latest) continue;
+
+            $results[] = [
+                'country' => $country->name,
+                'code' => $country->code,
+                'region' => $country->region,
+                'data' => $latest
+            ];
+        }
+
+        return response($results);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Display a listing of the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function single($code)
     {
-        //
+        $results = [];
+        $country = Country::whereCode($code)->single;
+
+        if (!$country) return [];
+
+        // Get the latest data for the country
+        $history = DailyStatistic::whereCountryId($country->id)->orderByDesc('date')->get();
+
+        if (!$history) return [];
+
+        $results = [
+            'country' => $country->name,
+            'code' => $country->code,
+            'region' => $country->region,
+            'data' => $history
+        ];
+
+        return response($results);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\DailyStatistic  $dailyStatistic
-     * @return \Illuminate\Http\Response
-     */
-    public function show(DailyStatistic $dailyStatistic)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\DailyStatistic  $dailyStatistic
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(DailyStatistic $dailyStatistic)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\DailyStatistic  $dailyStatistic
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, DailyStatistic $dailyStatistic)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\DailyStatistic  $dailyStatistic
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(DailyStatistic $dailyStatistic)
-    {
-        //
-    }
 }
